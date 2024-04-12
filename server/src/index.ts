a26b3d99ffaa8a993e82d0e0cbe6e67dcb62669c
PhamThuyDung-GitHub <20521214@gm.uit.edu.vn>
2024-04-12 13:16:37 +0700

    server

---
+++ server/src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+
+// create a server
+const app = express();
+
+// app.use(express.json());
+// app.use(express.urlencoded({extended: false}));
+
+app.post("/",
+ (req, res, next) => {
+  next();
+
+ },
+ (req, res) => {
+  // here we need data so that we can create new note/todo
+  console.log(req.body);
+  res.json({message: "I am listening!"})
+});
+
+// listen to some port
+app.listen(8000, () => {
+  console.log("listening");
+});
