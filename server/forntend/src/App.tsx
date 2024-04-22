c61a4a7cd6fcf0f6aae825abaa70a5fc9cbce95c
PhamThuyDung-GitHub <20521214@gm.uit.edu.vn>
2024-04-23 03:53:21 +0700

    update

---
+++ server/forntend/src/App.tsx
@@ -0,0 +1,27 @@
+
+import React from 'react'
+import './App.css'
+
+class SomeClass {
+
+}
+
+function App(props: {name: string}) {
+
+  return (
+      <div onClick={() => {
+        console.log("You just clicked");
+      }} className = "App">
+        <h1>Hello world {props.name}</h1>
+      </div>
+  )
+}
+
+//   return React.createElement("div", 
+//   {className: "App"},
+// React.createElement
+// ('h1', null, "Hello World"))
+// }
+
+export default App
+ 
