472526a130bc0f19e170da7cb105fc6cae948134
PhamThuyDung-GitHub <20521214@gm.uit.edu.vn>
2024-04-23 04:16:19 +0700

    update

--- server/forntend/src/App.tsx
+++ server/forntend/src/App.tsx
@@ -1,27 +1,10 @@
-
-import React from 'react'
-import './App.css'
-
-class SomeClass {
-
-}
-
-function App(props: {name: string}) {
-
-  return (
-      <div onClick={() => {
-        console.log("You just clicked");
-      }} className = "App">
-        <h1>Hello world {props.name}</h1>
-      </div>
-  )
-}
-
-//   return React.createElement("div", 
-//   {className: "App"},
-// React.createElement
-// ('h1', null, "Hello World"))
-// }
+const App = () => {
+  return (<div>
+    <h1>Note Application</h1>
+    <input type="text" />
+    <textarea></textarea>
+  </div>
+  );
+};
 
 export default App
- 
--- server/forntend/src/main.tsx
+++ server/forntend/src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import App from "./App";
+import "./index.css";
+
+
+const rootEl = document.getElementById("root") as 
+HTMLElement; 
+const root = createRoot(rootEl); // No need to cast to HTMLElement
+
+root.render(
   <React.StrictMode>
-    <App name = 'Anything'/>
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
