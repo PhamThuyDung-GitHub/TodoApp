00f6d73e889fa1f5eac85a4e5f49dfe32c83656f
PhamThuyDung-GitHub <20521214@gm.uit.edu.vn>
2024-04-24 21:52:43 +0700

    update

--- server/src/routers/note.ts
+++ server/src/routers/note.ts
@@ -1,18 +1,18 @@
 import { Router } from "express";
-import { create, getAllNotes, getSingleNotes, removeSingleNote, updateSingleNote } from "../controllers/note";
+import {
+  create,
+  getAllNotes,
+  getSingleNote,
+  removeSingleNote,
+  updateSingleNote,
+} from "../controllers/note";
 
-const router = Router()
-
-
+const router = Router();
 
 router.post("/create", create);
-
 router.patch("/:noteId", updateSingleNote);
-
 router.delete("/:noteId", removeSingleNote);
-
-router.get('/', getAllNotes);
-
-router.get('/:id', getSingleNotes);
+router.get("/", getAllNotes);
+router.get("/:id", getSingleNote);
 
 export default router;
