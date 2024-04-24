cf0c6cc37c9c6e9eb5675e975c15bd5a83c8f6a9
PhamThuyDung-GitHub <20521214@gm.uit.edu.vn>
2024-04-24 15:00:38 +0700

    update

--- server/forntend/src/App.tsx
+++ server/forntend/src/App.tsx
@@ -1,31 +1,42 @@
 import React, { useState, ChangeEventHandler } from "react";
 import NoteItem from "./components/NoteItem";
+import axios from "axios";
 
 // let title = "";
 const App = () => {
-  // const [title, setTitle] = useState("hello");
-  // const [description, setDescription] = useState("hello");
+  // const [title, setTitle] = useState("");
+  // const [description, setDescription] = useState("");
 
-  const [values, setValues]=useState({
+  const [values, setValues] = useState({
     title: "",
-    description: ""
-  })
+    description: "",
+  });
 
-  const handleChange: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement> = ({target}) => {
-    const {name, value} = target
-    setValues({...values, [name]: value})
-
-  }
+  const handleChange: ChangeEventHandler<
+    HTMLInputElement | HTMLTextAreaElement
+  > = ({ target }) => {
+    const { name, value } = target;
+    setValues({ ...values, [name]: value });
+  };
 
   return (
     <div className="max-w-3xl mx-auto space-y-6">
-      <form onSubmit={(evt) => {
-        evt.preventDefault();
-        console.log(values);
-      }} className="space-y-6 bg-white shadow-md rounded p-5">
+      <form
+        onSubmit={async (evt) => {
+          evt.preventDefault();
+          const { data } = await axios.post(
+            "http://localhost:8000/note/create",
+            {
+              title: values.title,
+              description: values.description,
+            }
+          );
+          console.log(data);
+        }}
+        className="space-y-6 bg-white shadow-md rounded p-5"
+      >
         <h1 className="font-semibold text-2xl text-center">Note Application</h1>
         <div>
-        
           <input
             placeholder="Title"
             type="text"
@@ -39,16 +50,13 @@ const App = () => {
           <textarea
             placeholder="Description"
             className="w-full border-b-2 border-gray-700 outline-none resize-none h-36"
-            value = {values.description}
+            value={values.description}
             onChange={handleChange}
             name="description"
           ></textarea>
         </div>
         <div className="text-right">
-          <button
-        
-            className="bg-blue-500 text-white px-5 py-2 rounded"
-          >
+          <button className="bg-blue-500 text-white px-5 py-2 rounded">
             Submit
           </button>
         </div>
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import "./db";
-import noteRouter from './routers/note';
+import cors from "cors";
 
+import noteRouter from "./routers/note";
 
 // create a server
 const app = express();
+
+app.use(cors());
 
 // this will parse post request coming from fetch.post() method
 app.use(express.json());
@@ -12,15 +15,14 @@ app.use(express.json());
 // this will parse post request coming from html form
 app.use(express.urlencoded({ extended: false }));
 
-
-/*"localhost:8000/note/create"
+/**
+ * 
+ * "localhost:8000/note/create"
 "localhost:8000/note"
 "localhost:8000/note/noteId"
 "localhost:8000/note/noteId"
 */
-
-app.use("/note",noteRouter);
-
+app.use("/note", noteRouter);
 
 // listen to some port
 app.listen(8000, () => {
